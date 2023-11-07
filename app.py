8a216f327dc3d35f0f640cf485461affeec7fa28
stephanevdb <mail@stephanevdb.com>
2023-11-07 09:35:37 +0100

    .

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import random
 import string
 import requests
+import os
+from time import sleep
+
+os.environ['CONNECT_IP'] = '1.1.1.1'
+
 
 worker_id =  ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 ipv4_capable = False
 ipv4_address = None
 ipv6_capable = False
 ipv6_address = None
+
+connect_ip = os.getenv('CONNECT_IP')
+
 
 try:
     ipv4_address = requests.get('https://api.ipify.org').text
@@ -25,4 +33,7 @@ except requests.exceptions.RequestException as e:
 print("worker_id: ", worker_id)
 print("ipv4_capable: ", ipv4_capable, "| ipv4_address: ", ipv4_address)
 print("ipv6_capable: ", ipv6_capable, "| ipv6_address: ", ipv6_address)
+print("connect_ip: ", connect_ip)
 
+while True:
+    sleep(1)
