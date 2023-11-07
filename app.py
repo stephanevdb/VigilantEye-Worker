cdbb354b90fa095892cb42e5322979893995437d
stephanevdb <mail@stephanevdb.com>
2023-11-07 10:29:28 +0100

    add connection init

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import requests
 import os
 from time import sleep
 
-#os.environ['MASTER_IP'] = '127.0.0.1'
-
 
 worker_id =  ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 ipv4_capable = False
@@ -14,6 +12,11 @@ ipv6_capable = False
 ipv6_address = None
 
 master_ip = os.getenv('MASTER_IP')
+if not master_ip:
+    master_ip = '127.0.0.1'
+master_port = os.getenv('MASTER_PORT')
+if not master_port:
+    master_port = 5000
 
 
 try:
@@ -28,13 +31,22 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error getting IPv6 address:", e)
 
-
+init_message = f"{worker_id},{ipv4_capable},{ipv4_address},{ipv6_capable},{ipv6_address}"
 
 print("worker_id: ", worker_id)
 print("ipv4_capable: ", ipv4_capable, "| ipv4_address: ", ipv4_address)
 print("ipv6_capable: ", ipv6_capable, "| ipv6_address: ", ipv6_address)
-print("master_ip: ", master_ip)
+print("master_ip: ", master_ip, "| master_port: ", master_port)
+
+try:
+    requests.post(f"http://{master_ip}:{master_port}/add_worker", data=init_message)
+    print("Worker added successfully")
+except requests.exceptions.RequestException as e:
+    print("Error adding worker:", e)
+
 print("Waiting for job...")
+
+
 
 while True:
     sleep(1)
