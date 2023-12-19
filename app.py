4453850f20e8e75171a5260ac4d76e5920464692
stephanevdb <mail@stephanevdb.com>
2023-12-19 10:14:34 +0100

    running 2 times

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+from flask import Flask, request
 import string
 import requests
 import os
@@ -6,10 +7,14 @@ from time import sleep
 
 
 worker_id =  ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+
 ipv4_capable = False
 ipv4_address = None
 ipv6_capable = False
 ipv6_address = None
+
+app = Flask(__name__)
+
 
 master_ip = os.getenv('MASTER_IP')
 
@@ -20,7 +25,6 @@ if ':' in master_ip:
 master_port = os.getenv('MASTER_PORT')
 if not master_port:
     master_port = 8666
-
 
 try:
     ipv4_address = requests.get('https://api.ipify.org').text
@@ -41,6 +45,19 @@ print("ipv4_capable: ", ipv4_capable, "| ipv4_address: ", ipv4_address)
 print("ipv6_capable: ", ipv6_capable, "| ipv6_address: ", ipv6_address)
 print("master_ip: ", master_ip, "| master_port: ", master_port)
 
+
+
+
+@app.route('/ping')
+def ping():  
+    return 'pong'
+
+@app.route('/job')
+def job():
+    return "job"
+  
+
+
 try:
     requests.post(f"http://{master_ip}:{master_port}/add_worker", data=init_message)
     print("Worker added successfully")
@@ -49,7 +66,5 @@ except requests.exceptions.RequestException as e:
 
 print("Waiting for job...")
 
-
-
-while True:
-    sleep(1)
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=8667)
